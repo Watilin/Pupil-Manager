10734a28c6ceb6d70b37695103ed934d05b3289d
Watilin <watilin@protonmail.com>
2018-06-11 18:53:10 +0200

    v1.2

---
+++ Pupil_Manager.meta.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name          Pupil Manager
+// @namespace     fr.kergoz-panic.watilin
+// @description   Outil pour gérer l’envoi et la réception d’élèves dans Teacher-Story.
+// @version       1.2
+//
+// @author        Watilin
+// @license       GPLv2; http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+// @supportURL    https://github.com/Watilin/Pupil-Manager/issues
+//
+// @icon          https://raw.githubusercontent.com/Watilin/Pupil-Manager/master/icon.png
+// @downloadURL   https://raw.githubusercontent.com/Watilin/Pupil-Manager/master/Pupil_Manager.user.js
+// @updateURL     https://raw.githubusercontent.com/Watilin/Pupil-Manager/master/Pupil_Manager.meta.js
+//
+// @include       http://teacher-story.com/*
+// @nocompat
+// @noframes
+//
+// @grant         GM_info
+// @grant         GM_xmlhttpRequest
+// @grant         GM_getResourceText
+// @grant         GM_getResourceURL
+// @grant         GM_getValue
+// @grant         GM_setValue
+//
+// @resource      ui-html           ui.html?=v1.2
+// @resource      ui-style          ui.css?v=1.2
+// @resource      artwork           artwork.png?v=1.2
+// ==/UserScript==
